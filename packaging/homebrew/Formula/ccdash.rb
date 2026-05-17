69fa0bc67820b78f754ead04cf99ca8a655d46ea
Carson Taylor <carsonjtaylor10@gmail.com>
2026-05-17 18:01:48 -0400

    formula: v0.3.0 sha256

--- packaging/homebrew/Formula/ccdash.rb
+++ packaging/homebrew/Formula/ccdash.rb
@@ -6,7 +6,7 @@ class Ccdash < Formula
   # Source-build formula. When precompiled release artifacts are hosted,
   # replace `url` and update `sha256`.
   url "https://github.com/cjtaylor10/ccdash/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "3ce15960e8d845b378d105fa081661164e24297a701f29e261d9e4b0f0570219"
   license "MIT"
 
   depends_on "rust" => :build
